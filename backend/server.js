8f49fc231d116ecb0366cc287ccd506164ea1fce
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-23 21:02:12 +0545

    Basic api setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js'
 dotenv.config();
 const  app= express();
+
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log( `Connected to database`)
@@ -12,3 +14,4 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 app.listen(3000,()=>{
     console.log( `Server listening at port 3000`);
 })
+app.use('/api/user',userRoutes)
