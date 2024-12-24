c46cbafd6a4081b0a7bbad329948d40a9912daa3
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-12-24 14:49:13 +0545

    signup routes,controller, and api setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+
+import User from '../models/user.model.js'
+import bcrpytjs from 'bcryptjs';
+
+export const signup=async (req,res)=>{
+    //console.log(req.body)
+  const {username, email,password}=req.body;
+  const hashedPassword= bcrpytjs.hashSync(password,10);
+  const newUser = User({username, email,password: hashedPassword});
+  try{
+   await newUser.save();
+   res.status(201).json({message:"New user Created successfully"});
+  }
+  catch(error){
+      res.status(500).json(error.message);
+  }
+}
